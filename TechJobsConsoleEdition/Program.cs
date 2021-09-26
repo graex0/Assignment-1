4bdcc4cd831d80f83481b18498fff8d9b1a3c3b9
graex0 <graex0@gmail.com>
2021-09-26 16:57:46 -0400

    first commit

---
+++ TechJobsConsoleEdition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechJobsConsoleEdition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
